54828bfd8664275829ec17286b8cd593a82cb7d6
Michail Eshmakov <mihail.eshmakov@yandex.ru>
2021-11-17 15:31:19 +0500

    init

---
+++ Napilnik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
